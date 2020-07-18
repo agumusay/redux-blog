56243fc1e0f0a3cff26d58fecb56a329d8a09211
agumusay <agumusay@protonmail.com>
2020-07-18 23:19:49 +0200

    bottom padding

--- src/App.js
+++ src/App.js
@@ -5,28 +5,29 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import ProductDetails from './components/ProductDetails';
 import Home from './components/Home';
 import data from './data.json';
-import { Menu, Image } from 'semantic-ui-react';
+import { Menu, Image, Icon } from 'semantic-ui-react';
 
 function App() {
-  function markUp() {
-    return (
-      <Router>
-        <Menu pointing secondary>
-        <Menu.Item icon="home" as={Link} to="/" />
-        </Menu>
+	function markUp() {
+		return (
+			<Router>
+				<Menu pointing secondary>
+					<Icon name='home' size='big' color='teal' />
+					<Menu.Header color='teal' content={<h2>Awesome Blog</h2>} />
+				</Menu>
 
-        <Image>
-          <img alt="" src='https://loremflickr.com/910/240/antalya?lock=16' />
-        </Image>
-      
-        <Switch>
-          <Route path={'/:slug'} render={(props) => <ProductDetails {...props} />} />
-          <Route path="/" render={(props) => <Home data={data} {...props} />} />
-        </Switch>
-      </Router>
-    );
-  }
-  return markUp();
+				<Image>
+					<img alt='' src='https://loremflickr.com/910/240/antalya?lock=16' />
+				</Image>
+
+				<Switch>
+					<Route path={'/:slug'} render={props => <ProductDetails {...props} />} />
+					<Route path='/' render={props => <Home data={data} {...props} />} />
+				</Switch>
+			</Router>
+		);
+	}
+	return markUp();
 }
 
 export default App;
